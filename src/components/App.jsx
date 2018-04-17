05ed830fe12c108ae5130b0e167b8952c73ff321
Taku <gcobs135102@gmail.com>
2018-04-17 11:12:38 +0800

    router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from '../store';
-import { plus, plusIfOdd } from '../actions/counter';
+import React, { Fragment } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Counter from './Counter';
 
-const mapStateToProps = state => ({
-  count: state.counter.count
-});
-
-const mapDispatchToProps = dispatch => ({
-  plusNum: num => () => dispatch(plus(num)),
-  plusNumIfOdd: num => () => dispatch(plusIfOdd(num))
-});
-
-const App = ({ count, plusNum, plusNumIfOdd }) => (
-  <main>
-    {count}
-    <button onClick={plusNum(1)}>+1</button>
-    <button onClick={plusNumIfOdd(1)}>+1 if odd</button>
-  </main>
+const App = () => (
+  <Fragment>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/counter/">Counter</Link>
+    </nav>
+    <Route exact strict path='/' render={() => <main>Home</main>} />
+    <Route exact strict path='/counter/' component={Counter} />
+  </Fragment>
 );
 
-App.propTypes = {
-  count: PropTypes.number.isRequired,
-  plusNum: PropTypes.func.isRequired,
-  plusNumIfOdd: PropTypes.func.isRequired
-};
-
-export default connect(App, { mapStateToProps, mapDispatchToProps });
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import { Provider } from './store';
 import App from './components/App';
 
+const history = createBrowserHistory();
+
 render(
   <Provider>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>
   ,
   document.getElementById('app')
